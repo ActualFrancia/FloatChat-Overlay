60f77cfd0d17de138f56c7e772f9feff29c2427d
Kali Francia <kali@francia.uk>
2023-12-15 16:46:20 -0500

    Initial Commit

---
+++ FloatChat Overlay/FloatChat_OverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatChat_OverlayApp.swift
+//  FloatChat Overlay
+//
+//  Created by Kali Francia on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FloatChat_OverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
